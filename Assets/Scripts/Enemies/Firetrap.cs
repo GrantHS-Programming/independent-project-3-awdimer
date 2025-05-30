30a0af4c8f7897f160a18e7ecacfac8bacebb48b
awidmer7246 <awidmer7246@student.pps.net>
2025-05-30 09:27:41 -0700

    running into som issues not sure how to fix but still making prgress. 2 traps done

--- Assets/Scripts/Enemies/Firetrap.cs
+++ Assets/Scripts/Enemies/Firetrap.cs
@@ -14,7 +14,7 @@ public class Firetrap : MonoBehaviour
     private bool triggered;
     private bool active;
     
-    private void Awakw()
+    private void Awake()
     {
         anim = GetComponent<Animator>();
         spriteRend = GetComponent<SpriteRenderer>();
@@ -24,22 +24,28 @@ public class Firetrap : MonoBehaviour
     {
         if(collision.tag == "Player")
             if(!triggered)
-            {
                 StartCoroutine(ActivateFiretrap());
-            }
+            
             if(active)
                 collision.GetComponent<Health>().TakeDamage(damage);
     }
     
     private IEnumerator ActivateFiretrap()
     {
+        //turn the sprite red to notify the player and trigger the trap
         triggered = true;
-        spriteRend.color = Color.red;
+        spriteRend.color = Color.red; 
+
+        //Wait for delay, activate trap, turn on animation, return color back to normal
         yield return new WaitForSeconds(activationDelay);
-        spriteRend.color = Color.white;
+        spriteRend.color = Color.white; //turn the sprite back to its initial color
         active = true;
+        anim.SetBool("activated", true);
+
+        //Wait until X seconds, deactivate trap and reset all variables and animator
         yield return new WaitForSeconds(activeTime);
         active = false;
         triggered = false;
+        anim.SetBool("activated", false);
     }
 }
